ed3550fc38a3ae781f0cb6eb1ef49ce1bc856d12
TomDotScott <tom.scott45456@gmail.com>
2020-10-31 17:29:22 +0000

    Basic Enemies

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + (moveSpeed * Time.deltaTime), transform.position.y);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("I HIT SOMETHING");
+        if (collision.gameObject.name.Contains("ChangeDirection"))
+        {
+            Debug.Log("I AM CHANGING DIRECTION");
+            moveSpeed *= -1;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("I TRIGGERED SOMETHING");
+        if (collision.gameObject.name.Contains("ChangeDirection"))
+        {
+            Debug.Log("I AM CHANGING DIRECTION");
+            moveSpeed *= -1;
+        }
+    }
+}
