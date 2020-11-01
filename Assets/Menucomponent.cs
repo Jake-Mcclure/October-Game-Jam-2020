a512f964538e785c5bbc2193c5830783faabecd7
Jake-Mcclure <62794175+Jake-Mcclure@users.noreply.github.com>
2020-11-01 15:55:51 +0000

    Main Menu and splash screen

---
+++ Assets/Menucomponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Menucomponent : MonoBehaviour {
+
+    //all these are used to load a specific scene from the build settings
+	public void playgame()
+    {
+        SceneManager.LoadScene("Level1");
+        
+    }
+    
+    //this is used to exit the application
+    public void Exitgame()
+    {
+        Application.Quit();
+    }
+
+
+}
